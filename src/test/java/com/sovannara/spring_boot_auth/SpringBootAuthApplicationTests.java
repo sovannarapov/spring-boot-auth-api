ca5a5c3756a3932216349d1900e7a9f2c994cf18
sovannarapov <sovannarapov9@gmail.com>
2024-12-25 22:31:14 +0700

    refactor: update SpringBootAuthApplicationTests.java

--- src/test/java/com/sovannara/spring_boot_auth/SpringBootAuthApplicationTests.java
+++ src/test/java/com/sovannara/spring_boot_auth/SpringBootAuthApplicationTests.java
@@ -1,16 +1,12 @@
 package com.sovannara.spring_boot_auth;
 
-import com.sovannara.spring_boot_auth.config.SecurityConfigTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-@Import({SpringBootAuthApplication.class, SecurityConfigTest.class})
 class SpringBootAuthApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 
 }
