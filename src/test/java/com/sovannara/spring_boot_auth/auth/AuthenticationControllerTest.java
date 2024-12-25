461760c812eb83836b707d380c82bee51452f1d0
sovannarapov <sovannarapov9@gmail.com>
2024-12-25 23:22:52 +0700

    refactor: update AuthenticationControllerTest.java

--- src/test/java/com/sovannara/spring_boot_auth/auth/AuthenticationControllerTest.java
+++ src/test/java/com/sovannara/spring_boot_auth/auth/AuthenticationControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthenticationController.class)
-@Import(SecurityConfigTest.class)
+@Profile("test")
 public class AuthenticationControllerTest {
 
     @Autowired
