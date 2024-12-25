1a1af38c484ff4f0e27e5d97617a3c39b62afd0c
sovannarapov <sovannarapov9@gmail.com>
2024-12-25 23:19:30 +0700

    refactor: update SecurityConfigTest.java

--- src/test/java/com/sovannara/spring_boot_auth/config/SecurityConfigTest.java
+++ src/test/java/com/sovannara/spring_boot_auth/config/SecurityConfigTest.java
@@ -2,11 +2,13 @@ package com.sovannara.spring_boot_auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@Profile("test")
 public class SecurityConfigTest {
 
     @Bean
