533d7e96c7f478e2bbef617839634db39306fdcc
sovannarapov <sovannarapov9@gmail.com>
2024-12-27 17:03:12 +0700

    feat: load environment variables in application

--- src/main/java/com/sovannara/spring_boot_auth/SpringBootAuthApplication.java
+++ src/main/java/com/sovannara/spring_boot_auth/SpringBootAuthApplication.java
@@ -1,12 +1,20 @@
 package com.sovannara.spring_boot_auth;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class SpringBootAuthApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", Objects.requireNonNull(dotenv.get("DB_URL")));
+		System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
+		System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+
 		SpringApplication.run(SpringBootAuthApplication.class, args);
 	}
 
