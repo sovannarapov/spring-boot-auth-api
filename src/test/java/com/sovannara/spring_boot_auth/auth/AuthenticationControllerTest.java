f3149a0631fb0f009a24089fa2037f626675c6d0
sovannarapov <sovannarapov9@gmail.com>
2025-01-01 00:34:34 +0700

    feat: add new testing for confirm user and refresh token

--- src/test/java/com/sovannara/spring_boot_auth/auth/AuthenticationControllerTest.java
+++ src/test/java/com/sovannara/spring_boot_auth/auth/AuthenticationControllerTest.java
@@ -3,23 +3,24 @@ package com.sovannara.spring_boot_auth.auth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sovannara.spring_boot_auth.config.SecurityConfigTest;
 import com.sovannara.spring_boot_auth.jwt.JwtService;
-import com.sovannara.spring_boot_auth.user.Role;
-import com.sovannara.spring_boot_auth.user.User;
 import com.sovannara.spring_boot_auth.user.UserService;
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AuthenticationController.class)
 @Import(SecurityConfigTest.class)
@@ -40,27 +41,66 @@ public class AuthenticationControllerTest {
     @MockBean
     private UserService userService;
 
-    private final List<User> users = new ArrayList<>();
-
-    @BeforeEach
-    void setUp() {
-        users.add(new User(
-                1,
-                "John",
-                "Doe",
-                "johndoe@gmail.com",
-                "password",
-                Role.USER
-        ));
-    }
-
     @Test
     void shouldRegisterNewUser() throws Exception {
-        var user = new User(2, "John", "Wick", "johnwick@gmail.com", "password", Role.USER);
+        RegisterRequestDto user = new RegisterRequestDto("John", "Wick", "johnwick@gmail.com", "password");
+        AuthenticationResponseDto responseDto = new AuthenticationResponseDto("accessToken", "refreshToken");
+
+        when(authenticationService.register(any(RegisterRequestDto.class))).thenReturn(responseDto);
+
         mockMvc.perform(post("/api/v1/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.access_token").value("accessToken"))
+                .andExpect(jsonPath("$.refresh_token").value("refreshToken"));
+    }
+
+    @Test
+    void shouldLoginUser() throws Exception {
+        LoginRequestDto user = new LoginRequestDto("johnwick@gmail.com", "password");
+        AuthenticationResponseDto responseDto = new AuthenticationResponseDto("accessToken", "refreshToken");
+
+        when(authenticationService.login(any(LoginRequestDto.class))).thenReturn(responseDto);
+
+        mockMvc.perform(post("/api/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.access_token").value("accessToken"))
+                .andExpect(jsonPath("$.refresh_token").value("refreshToken"));
+    }
+
+    @Test
+    void shouldConfirmUser() throws Exception {
+        String token = "sampleToken";
+        String responseMessage = "<h1>Your account has been successfully activated</h1>";
+
+        when(authenticationService.confirm(token)).thenReturn(responseMessage);
+
+        mockMvc.perform(get("/api/v1/auth/confirm")
+                        .param("token", token))
+                .andExpect(status().isOk())
+                .andExpect(content().string(responseMessage));
+    }
+
+    @Test
+    void shouldRefreshToken() throws Exception {
+        String refreshToken = "Bearer sampleRefreshToken";
+        AuthenticationResponseDto responseDto = new AuthenticationResponseDto("newAccessToken", "newRefreshToken");
+
+        when(jwtService.extractUsername(any(String.class))).thenReturn("johnwick@gmail.com");
+        doAnswer(invocation -> {
+            HttpServletResponse response = invocation.getArgument(1);
+            new ObjectMapper().writeValue(response.getOutputStream(), responseDto);
+            return null;
+        }).when(authenticationService).refreshToken(any(HttpServletRequest.class), any(HttpServletResponse.class));
+
+        mockMvc.perform(post("/api/v1/auth/refresh-token")
+                        .header(HttpHeaders.AUTHORIZATION, refreshToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.access_token").value("newAccessToken"))
+                .andExpect(jsonPath("$.refresh_token").value("newRefreshToken"));
     }
 
 }
