12a4a83bd809ad8d35b6c4cac6547291785d338b
sovannarapov <sovannarapov9@gmail.com>
2025-01-12 21:57:54 +0700

    refactor: update security config

--- src/main/java/com/sovannara/spring_boot_auth/config/SecurityConfig.java
+++ src/main/java/com/sovannara/spring_boot_auth/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         req -> req.requestMatchers(
-                                        "/api/v*/auth/**"
+                                        "/api/auth/**"
                                 )
                                 .permitAll()
                                 .anyRequest()
