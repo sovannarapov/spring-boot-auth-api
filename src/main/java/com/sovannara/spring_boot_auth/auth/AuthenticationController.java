67e94921414841fb6d7ef0b202ad69b8077a3f28
sovannarapov <sovannarapov9@gmail.com>
2025-01-12 21:56:33 +0700

    refactor: update authentication controller

--- src/main/java/com/sovannara/spring_boot_auth/auth/AuthenticationController.java
+++ src/main/java/com/sovannara/spring_boot_auth/auth/AuthenticationController.java
@@ -1,6 +1,10 @@
 package com.sovannara.spring_boot_auth.auth;
 
+import com.sovannara.spring_boot_auth.auth.dto.AuthenticationResponseDto;
+import com.sovannara.spring_boot_auth.auth.dto.LoginRequestDto;
+import com.sovannara.spring_boot_auth.auth.dto.RegisterRequestDto;
 import com.sovannara.spring_boot_auth.exception.ApiResponse;
+import com.sovannara.spring_boot_auth.user.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,38 +13,29 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     private final AuthenticationService service;
 
     @PostMapping("/register")
-    ApiResponse<AuthenticationResponseDto> register(
-            @RequestBody RegisterRequestDto registerRequestDto
-    ) {
+    ApiResponse<User> register(@RequestBody RegisterRequestDto registerRequestDto) {
         return service.register(registerRequestDto);
     }
 
     @PostMapping("/login")
-    ApiResponse<AuthenticationResponseDto> login(
-            @RequestBody LoginRequestDto loginRequestDto
-    ) {
+    ApiResponse<AuthenticationResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
         return service.login(loginRequestDto);
     }
 
     @GetMapping("/confirm")
-    String confirm(
-            @RequestParam String token
-    ) {
+    String confirm(@RequestParam String token) {
         return service.confirm(token);
     }
 
     @PostMapping("/refresh-token")
-    void refreshToken(
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) throws IOException {
+    void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
         service.refreshToken(request, response);
     }
 
