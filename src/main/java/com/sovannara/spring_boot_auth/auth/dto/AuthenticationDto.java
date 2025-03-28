cc8c3ad075e97a5d37c0f5a4c62142e14115d3ca
sovannarapov <sovannarapov9@gmail.com>
2025-03-28 23:40:44 +0700

    refactor: rename file

--- src/main/java/com/sovannara/spring_boot_auth/auth/dto/AuthenticationDto.java
+++ src/main/java/com/sovannara/spring_boot_auth/auth/dto/AuthenticationDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class AuthenticationResponseDto {
+public class AuthenticationDto {
 
     @JsonProperty("access_token")
     private String accessToken;
