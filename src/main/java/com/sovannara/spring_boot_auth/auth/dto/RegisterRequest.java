b853623de6a60d39f16d5a8da5d2eab8fc1d07ab
sovannarapov <sovannarapov9@gmail.com>
2025-03-28 23:21:09 +0700

    refactor: rename file

--- src/main/java/com/sovannara/spring_boot_auth/auth/dto/RegisterRequest.java
+++ src/main/java/com/sovannara/spring_boot_auth/auth/dto/RegisterRequest.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class RegisterRequestDto {
+public class RegisterRequest {
 
     private String firstname;
 
