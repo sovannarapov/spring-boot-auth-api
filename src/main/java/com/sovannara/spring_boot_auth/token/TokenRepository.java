1136f578a14a2fe837d53f405bf74fff802c4842
sovannarapov <sovannarapov9@gmail.com>
2025-03-28 23:24:56 +0700

    refactor: update datatype

--- src/main/java/com/sovannara/spring_boot_auth/token/TokenRepository.java
+++ src/main/java/com/sovannara/spring_boot_auth/token/TokenRepository.java
@@ -5,17 +5,18 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface TokenRepository extends JpaRepository<Token, Integer> {
+public interface TokenRepository extends JpaRepository<Token, UUID> {
 
     @Query(value = """
       select t from Token t inner join User u\s
       on t.user.id = u.id\s
       where u.id = :id and (t.expired = false or t.revoked = false)\s
       """)
-    List<Token> findAllValidTokenByUser(Integer id);
+    List<Token> findAllValidTokenByUser(UUID id);
 
     Optional<Token> findByToken(String token);
 
-    Optional<Token> findByUserIdAndTokenType(Integer id, TokenType tokenType);
+    Optional<Token> findByUserIdAndTokenType(UUID id, TokenType tokenType);
 }
